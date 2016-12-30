e9d4f54f2e98670fb4d067a340123bf80b81d531
peterszerzo <szerzo.peter@gmail.com>
2016-12-30 14:12:47 +0100

    Remove lots of bloat. Set up minimal build

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-require('babel/register');
-
-var gulp = require('gulp');
-
-require('./gulp_tasks/spec.js');
-require('./gulp_tasks/css.js');
-require('./gulp_tasks/js.js');
-require('./gulp_tasks/gh_pages.js');
---
+++ website.sh
@@ -0,0 +1,1 @@
+cat website/components/init.jsx website/components/general/**/*.jsx website/components/route_handlers/**/*.jsx website/routes/index.jsx > dist/1.jsx
